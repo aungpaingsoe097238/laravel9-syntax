74fbc241b1efbc0b3fd0129c1f429d752e2b710e
Aung Paing Soe <aungpaingsoe097238@gmail.com>
2022-09-08 20:25:16 -0700

    Database transaction

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,7 @@ use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -51,38 +52,51 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        $post = new Post();
-        $post->title = $request->title;
-        $post->slug = Str::slug($request->title);
-        $post->description = $request->description;
-        $post->excerpt = Str::words(50,$request->description,' .....');
-        $post->category_id = $request->category;
-        $post->user_id = Auth::id();
 
-        if($request->hasFile('featured_image')){
-            $newName = uniqid().'_featured_image.'.$request->file('featured_image')->extension();
-            $request->file('featured_image')->storeAs('public',$newName);
-            $post->featured_image = $newName;
-        }
+        try{
 
-        $post->save();
+            DB::beginTransaction();
 
-        $savePhotos = [];
-        foreach ($request->photos as $key=>$photo){
-            $newName = uniqid().'_photo.'.$photo->extension();
-            $photo->storeAs('public',$newName);
-            $savePhotos[$key] = [
-                'post_id' => $post->id,
-                'name'    => $newName
-            ];
-        }
+            // Start
+            $post = new Post();
+            $post->title = $request->title;
+            $post->slug = Str::slug($request->title);
+            $post->description = $request->description;
+            $post->excerpt = Str::words(50,$request->description,' .....');
+            $post->category_id = $request->category;
+            $post->user_id = Auth::id();
 
-        Photo::insert($savePhotos);
+            if($request->hasFile('featured_image')){
+                $newName = uniqid().'_featured_image.'.$request->file('featured_image')->extension();
+                $request->file('featured_image')->storeAs('public',$newName);
+                $post->featured_image = $newName;
+            }
+
+            $post->save();
+
+            $savePhotos = [];
+            foreach ($request->photos as $key=>$photo){
+                $newName = uniqid().'_photo.'.$photo->extension();
+                $photo->storeAs('public',$newName);
+                $savePhotos[$key] = [
+                    'post_id' => $post->id,
+                    'name'    => $newName
+                ];
+            }
+
+            Photo::insert($savePhotos);
 
 //        $photos = new Photo();
 //        $photos->post_id = $post->id;
 //        $photos->name = $newName;
 //        $photos->save();
+            // End
+
+            DB::commit();
+
+        }catch (\Exception $error){
+            DB::rollBack();
+        }
 
         return redirect()->route('post.index');
     }
